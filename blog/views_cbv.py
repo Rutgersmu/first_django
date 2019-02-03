112d21e7aab3afe55f21cee184a025b8d14455fc
unknown <softstep1221@gmail.com>
2019-02-04 00:24:20 +0900

    CBV : DetailView 적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,8 +1,9 @@
 from django import forms #원래는 블로그에 폼즈 파일 따로 구현해야 하지만 설명을 위해....
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from .models import Post
 
 post_list = ListView.as_view(model=Post, paginate_by=10)
+post_detail = DetailView.as_view(model=Post)
 
 
 class PostForm(forms.ModelForm):
