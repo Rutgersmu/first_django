4d6b1faea8066d8c224e4ce6144275e0f37e9047
unknown <softstep1221@gmail.com>
2019-02-04 00:21:07 +0900

    CBV : ListView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views_cbv.post_list, name='post_list'),
     # re_path(r'^(?P<id>\d+)/$', views.post_detail),
     path('<int:id>/', views.post_detail, name='post_detail'),
     path('new', views.post_new, name='post_new'),
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,6 +1,8 @@
 from django import forms #원래는 블로그에 폼즈 파일 따로 구현해야 하지만 설명을 위해....
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Post
+
+post_list = ListView.as_view(model=Post, paginate_by=10)
 
 
 class PostForm(forms.ModelForm):
