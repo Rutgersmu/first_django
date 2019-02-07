7884e4e524b08e4e190a87d38eafb5966fe8c21a
unknown <softstep1221@gmail.com>
2019-02-07 18:52:58 +0900

    로그인/로그아웃 링크에 next 인자 지정

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.login, name='login', kwargs={'template_name': 'accounts/login_form.html'}),
-    path('logout/', auth_views.logout, name='logout', kwargs={'next_page': settings.LOGIN_URL}),
+    path('login/', auth_views.LoginView, name='login', kwargs={'template_name': 'accounts/login_form.html'}),
+    path('logout/', auth_views.LogoutView, name='logout', kwargs={'next_page': settings.LOGIN_URL}),
     path('profile/', views.profile, name='profile'),
 ]
